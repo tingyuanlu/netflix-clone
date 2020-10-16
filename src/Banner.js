60e357e93aa816919018334530547960a9e8a095
tingyuanlu <tingyuan.lu43@gmail.com>
2020-10-15 23:21:02 -0400

    banner update

--- src/Banner.js
+++ src/Banner.js
@@ -17,24 +17,32 @@ function Banner() {
     }
     fetchData();
   }, []);
-  console.log(movie);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('htttps://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
