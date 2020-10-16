5fa59459149b121df86295e65cae3f04a0b9475f
tingyuanlu <tingyuan.lu43@gmail.com>
2020-10-16 13:52:01 -0400

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Row from "./Row";
 import "./App.css";
 import requests from "./request";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORGINALS"
